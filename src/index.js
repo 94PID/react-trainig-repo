f9178f50ca86d03593dedfe466fc803f52fefed0
Rapid <datagram33@gmail.com>
2019-11-12 16:04:48 +0100

    added nav bar and some simple styles

--- src/index.js
+++ src/index.js
@@ -9,12 +9,41 @@ import thunkMiddleware from "redux-thunk";
 import monitorReducerEnhancer from "./enhancers/monitorReducer";
 import rootReducer from "./reducers";
 import { composeWithDevTools } from 'redux-devtools-extension'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "reactstrap/es/Navbar";
+import Nav from "reactstrap/es/Nav";
+import {HashRouter, Link, Route} from "react-router-dom";
 
 const store = configureStore();
 
+const NavBarWrapper = () => {
+    return (
+        <Navbar className={"navbar"} expand={"md"}>
+            <i className="fas fa-home navbar-item icon"/>
+            <Nav>
+                <Link className={"navbar-item "} to={"/profile"}>Profile</Link>
+                <Link className={"navbar-item"} to={"/gallery"}>Gallery</Link>
+            </Nav>
+        </Navbar>
+    )
+};
+
+const Routes = () => {
+    return (
+        <>
+            <Route exact path={"/"} component={App}/>
+            <Route path={"/profile"} render={() => <div>profile</div>} />
+            <Route path={"/gallery"} render={() => <div>gallery</div>} />
+        </>
+    )
+};
+
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <HashRouter>
+            <NavBarWrapper/>
+            <Routes/>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 
